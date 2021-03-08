6d015127e7f593e2360170fac5d675662e2469a1
Abrar <abrar@Abrars-MacBook-Pro.local>
2021-03-08 20:18:19 +0300

    solved task 2

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ const cors = require ("cors");
 const app =express();
 app.use(cors());
 
-const data = require ("./data");
+let data = require ("./data");
 app.get("/data", (req,res) => {
     res.json(data);
 })
+// delete id=1
+app.delete("/data/:dataId", (req,res) => {
+    const {dataId} = req.params;
+    //we need to find the product before we delete it
+    //we add + before dataId to convert it to a number
+    const foundData = data.find((cv) => cv.id ===+dataId);
+    if (foundData) {
+        data = data.filter((cv) => cv.id !== +foundData);
+        res.status(204).end();
+        //To end a response without sending anything back we will use the end method.
+    } else {
+        res.status(404);
+        res.json({ message: "A product with this ID doesn't Exist"});
+    }
+
+});
 
 app.get("/", (req,res) => {
     console.log("Hello");
